692ad36abb643262937d9769367087d7476ab77c
BIG_whale <f411024@gmail.com>
2018-06-23 18:01:42 +0800

    demerge text-based

--- nccu_course_query/querysys/admin.py
+++ nccu_course_query/querysys/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, Teacher, ClassTime, Department, CollegeProgram, user
+from .models import Course, Teacher, ClassTime, Department, CollegeProgram
 # Register your models here.
 
 admin.site.register(Course)
@@ -7,5 +7,3 @@ admin.site.register(Department)
 admin.site.register(Teacher)
 admin.site.register(ClassTime)
 admin.site.register(CollegeProgram)
-
-admin.site.register(user)
